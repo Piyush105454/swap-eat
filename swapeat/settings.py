7f63262b51dbca121d6acebd4bf380b9462a1a7e
Piyush105454 <piyushmodi812@gmail.com>
2025-01-09 13:02:29 +0530

    Update settings.py

--- swapeat/settings.py
+++ swapeat/settings.py
@@ -123,3 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ALLOWED_HOSTS = ['static-bessy-piyush105454-65e6965b.koyeb.app', 'localhost', '127.0.0.1']
+
