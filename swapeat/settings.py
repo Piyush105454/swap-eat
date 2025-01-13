99cc34836ef21c224a95b8ce7482bfc34fa5f0ce
Piyush105454 <piyushmodi812@gmail.com>
2025-01-14 01:13:08 +0530

    Update settings.py

--- swapeat/settings.py
+++ swapeat/settings.py
@@ -117,12 +117,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static/images")]  
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static/css")]
-MEDIA_URL = '/media/'
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static/images"),
+    os.path.join(BASE_DIR, "static/css"),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
+# Media files
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
