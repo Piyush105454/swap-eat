eccb3bb34a59b1f0c238fe67ac177f1eebe938b0
Piyush105454 <piyushmodi812@gmail.com>
2025-01-13 13:20:16 +0530

    Update settings.py

--- swapeat/settings.py
+++ swapeat/settings.py
@@ -118,6 +118,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static/images")]  
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static/css")]
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
