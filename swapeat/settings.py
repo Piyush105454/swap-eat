98369e6c3d0b775d8a630171ba961b66fc987c40
Piyush105454 <piyushmodi812@gmail.com>
2025-01-14 15:12:24 +0530

    Update settings.py

--- swapeat/settings.py
+++ swapeat/settings.py
@@ -123,7 +123,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static/images"),
     os.path.join(BASE_DIR, "static/css"),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Media files
 MEDIA_URL = '/media/'
