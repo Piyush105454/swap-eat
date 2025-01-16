e684743f295b320d53bf0ef8a39228b91a55b02a
Piyush105454 <piyushmodi812@gmail.com>
2025-01-16 16:10:15 +0530

    Update views.py

--- swapfood/views.py
+++ swapfood/views.py
@@ -6,7 +6,18 @@ from django.core.files.storage import default_storage
 # Create your views here.
 from django.conf import settings
 from .models import user
+from swapfood.models import Mychats
 # Create your views here.
+@login_required
+def index(request):
+    frnd_name = request.GET.get('user',None)
+    mychats_data = None
+    if frnd_name:
+        if User.objects.filter(username=frnd_name).exists() and Mychats.objects.filter(me=request.user,frnd=User.objects.get(username=frnd_name)).exists():
+            frnd_ = User.objects.get(username=frnd_name)
+            mychats_data = Mychats.objects.get(me=request.user,frnd=frnd_).chats
+    frnds = User.objects.exclude(pk=request.user.id)
+    return render(request,'index.html',{'my':mychats_data,'chats': mychats_data,'frnds':frnds})
 @login_required(login_url='login')
 def HomePage(request):
     return render (request,'home.html')
