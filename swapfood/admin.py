7687fea512395e33a6c7a2d4cd737f3bdc9b304e
Piyush105454 <piyushmodi812@gmail.com>
2025-01-16 20:15:36 +0530

    Update admin.py

--- swapfood/admin.py
+++ swapfood/admin.py
@@ -1,9 +1,1 @@
 from django.contrib import admin
-
-# Register your models here.
-from swapfood.models import Mychats
-# Register your models here.
-
-@admin.register(Mychats)
-class MychatAdmin(admin.ModelAdmin):
-    list_display = ('id','me','frnd','chats')
