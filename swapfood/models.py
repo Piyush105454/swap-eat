526514f97a330534a641e39058bdb524ae1fffb1
Piyush105454 <piyushmodi812@gmail.com>
2025-01-16 16:07:37 +0530

    Update models.py

--- swapfood/models.py
+++ swapfood/models.py
@@ -3,5 +3,13 @@ from django.db import models
 # Create your models here.
 class user(models.Model):
     pic = models.ImageField(upload_to="profiles")
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
 
+
+class Mychats(models.Model):
+    me = models.ForeignKey(to=User,on_delete=models.CASCADE,related_name='it_me')
+    frnd = models.ForeignKey(to=User,on_delete=models.CASCADE,related_name='my_frnd')
+    chats = models.JSONField(default=dict)
 # Create your models here.
