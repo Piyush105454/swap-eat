6b5049deebd50fd6c9e673a7490ed88b93e0bf93
Piyush105454 <piyushmodi812@gmail.com>
2025-01-19 18:34:29 +0530

    Update views.py

--- swapfood/views.py
+++ swapfood/views.py
@@ -1,4 +1,4 @@
-intfrom django.shortcuts import render,HttpResponse,redirect
+from django.shortcuts import render,HttpResponse,redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
