df0b6fc42d8595fe37745005bb1de4635b9ad4c4
Piyush105454 <piyushmodi812@gmail.com>
2025-01-19 10:28:31 +0530

    Update asgi.py

--- swapeat/asgi.py
+++ swapeat/asgi.py
@@ -1,19 +1,19 @@
-
 import os
-
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
-from swapfood import routing
-
+from channels.auth import AuthMiddlewareStack
+from swapfood.routing import websocket_urlpatterns  # Ensure this import path is correct
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'swapeat.settings')
 
+# Initialize Django ASGI application early to ensure the apps are loaded
 django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    "websocket": URLRouter(
-        routing.websocket_urlpatterns
-    )
-}) 
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            websocket_urlpatterns
+        )
+    ),
+})
