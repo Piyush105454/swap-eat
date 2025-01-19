a07e42f7ffbe7d561858b0da31ccd9c7c6b88213
Piyush105454 <piyushmodi812@gmail.com>
2025-01-19 09:58:30 +0530

    Update settings.py

--- swapeat/settings.py
+++ swapeat/settings.py
@@ -90,14 +90,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)],
-        },
-    },
-}
 
 
 # Password validation
