2e5efa22e28d3f405ba0d74f6eb63417cb548f93
Piyush105454 <piyushmodi812@gmail.com>
2025-01-22 16:12:38 +0530

    Update urls.py

--- swapeat/urls.py
+++ swapeat/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('home2/explore/', views.explore, name='explore'),
     path('home2/postmeal/', views.postmeal, name='postmeal'),
     path('home2/chat/', views.chat, name='chat'),
+    path('home2/post/', views.post, name='post'),
     
     path('home2/Home/', views.Home, name='Home'),
     path('admin/', admin.site.urls),
