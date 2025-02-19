5d0966c1a998b31ce3cd38912aab561729d57171
Piyush105454 <piyushmodi812@gmail.com>
2025-02-19 16:20:51 +0530

    Update urls.py

--- swapeat/urls.py
+++ swapeat/urls.py
@@ -5,10 +5,10 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('home2/explore/', views.explore, name='explore'),
-    path('home2/postmeal/', views.postmeal, name='postmeal'),
-    path('home2/chat/', views.chat, name='chat'),
-    path('home2/post/', views.post, name='post'),
+    path('post/explore/', views.explore, name='explore'),
+    path('post/postmeal/', views.postmeal, name='postmeal'),
+    path('post/chat/', views.chat, name='chat'),
+    path('post/', views.post, name='post'),
     path("chat-api/", views.chat_api, name="chat_api"),
     path('verify-otp/<int:user_id>/', views.VerifyOTP, name='verify_otp'),
     path('home2/Home/', views.Home, name='Home'),
@@ -18,7 +18,7 @@ urlpatterns = [
     
 
     path('signup/', views.SignupPage, name='signup'),  # Renamed for clarity
-    path('home2/', views.HomePage2, name='home2'),
+    path('post/home2/', views.HomePage2, name='home2'),
     path('login/', views.LoginPage, name='login'),
     path('', views.HomePage, name='home'),  # Fixed trailing slash consistency
     path('home/logout/', views.LogoutPage, name='logout'),
