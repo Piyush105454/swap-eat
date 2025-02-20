edfd9b5f2e84b7b6f3ba0e8aa81d028e6e31d1ec
Piyush105454 <piyushmodi812@gmail.com>
2025-02-20 15:01:53 +0530

    Update forms.py

--- swapfood/forms.py
+++ swapfood/forms.py
@@ -1,7 +1,16 @@
 from django import forms
 from .models import FoodPost
-
+from .models import Invitation
 class FoodPostForm(forms.ModelForm):
     class Meta:
         model = FoodPost
         fields = ['photo', 'latitude', 'longitude']
+# Invitation form
+class InvitationForm(forms.ModelForm):
+    class Meta:
+        model = Invitation
+        fields = ['email']
+
+# Search form
+class SearchForm(forms.Form):
+    query = forms.CharField(max_length=100, required=True)
