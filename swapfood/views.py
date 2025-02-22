c25f6f50d208df6c1568a14d0826bfb4ec219ae3
Piyush105454 <piyushmodi812@gmail.com>
2025-02-22 09:05:41 +0530

    Update views.py

--- swapfood/views.py
+++ swapfood/views.py
@@ -27,7 +27,7 @@ def post_food(request):
 
 
 def map_view(request):
-    food_posts = list(FoodPost.objects.values("latitude", "longitude","photo","created_at"))
+    food_posts = list(FoodPost.objects.values("latitude", "longitude","photo","created_at","id"))
 
     # Convert datetime objects to string
     for post in food_posts:
