77d693455f0913301c30c7df780d2d5e93dc816e
Piyush105454 <piyushmodi812@gmail.com>
2025-02-24 00:45:38 +0530

    Update urls.py

--- swapfood/urls.py
+++ swapfood/urls.py
@@ -8,8 +8,5 @@ urlpatterns = [
     # Route to open messages with username input
     path('chat/<str:room_name>/<str:username>/', views.MessageView, name='chat_with_user'),
 
-    # Search users
-    path('search_users/', views.search_users, name='search_users'),
-
-    # Other URLs...
+    
 ]
