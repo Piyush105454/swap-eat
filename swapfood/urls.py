2d84e16af047b2c0bbb20c8f8d6ad09bdf016a7a
Piyush105454 <piyushmodi812@gmail.com>
2025-02-24 00:05:05 +0530

    Update urls.py

--- swapfood/urls.py
+++ swapfood/urls.py
@@ -2,6 +2,10 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.MessageView, name='MessageView'),
+    # Route to open the chat page (messages.html)
+    path('chat/', views.chat, name='chat'),
+
+    # Route to open messages with username input
+    path('chat/<str:room_name>/<str:username>/', views.MessageView, name='room'),
     
 ]
