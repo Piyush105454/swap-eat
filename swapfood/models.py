e455db211b9725d528f5c81c9d2000d1a0d39dba
Piyush105454 <piyushmodi812@gmail.com>
2025-02-24 20:47:27 +0530

    Update models.py

--- swapfood/models.py
+++ swapfood/models.py
@@ -2,6 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class PrivateChat(models.Model):
+    user1 = models.ForeignKey(User, related_name="chat_user1", on_delete=models.CASCADE)
+    user2 = models.ForeignKey(User, related_name="chat_user2", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def get_chat_room_name(self):
+        return f"chat_{min(self.user1.username, self.user2.username)}_{max(self.user1.username, self.user2.username)}"
+
+
 class UserOTP(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     otp = models.CharField(max_length=6)
