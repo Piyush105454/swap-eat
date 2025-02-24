eec0257aadddefb44d2d734dcbd2c26899843913
Piyush105454 <piyushmodi812@gmail.com>
2025-02-24 09:58:43 +0530

    Update urls.py

--- swapfood/urls.py
+++ swapfood/urls.py
@@ -1,12 +1,7 @@
 from django.urls import path
-from . import views
+from .views import search_users, MessageView
 
 urlpatterns = [
-    # Open chat page where user can enter a username to start chatting
-    path('chat/', views.chat, name='chat'),
-
-    # Route to open messages with username input
-    path('chat/<str:room_name>/<str:username>/', views.MessageView, name='chat_with_user'),
-    path("search_users/", views.search_users, name="search_users"),
-    
+    path('search_users/', search_users, name='search_users'),
+    path('chat/<str:username>/', MessageView, name='chat_with_user'),
 ]
