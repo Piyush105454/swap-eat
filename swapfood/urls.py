92989fb3580c324fed3901b6c830d3e11ce08b7f
Piyush105454 <piyushmodi812@gmail.com>
2025-02-24 21:57:13 +0530

    Update urls.py

--- swapfood/urls.py
+++ swapfood/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("send_message/", views.send_message, name="send_message"),
+    
     path("search_users/", views.search_users, name="search_users"),
     path("chat/<int:receiver_id>/", views.chat_view, name="chat_view"),
 ]
