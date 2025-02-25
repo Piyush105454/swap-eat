8184bb15b14da74583b042446cf447b2df7ca858
Piyush105454 <piyushmodi812@gmail.com>
2025-02-25 11:51:25 +0530

    Update urls.py

--- swapfood/urls.py
+++ swapfood/urls.py
@@ -1,7 +1,7 @@
-from . import views
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('', views.CreateRoom, name='create-room'),
+    path('create-room/', views.CreateRoom, name='create-room'),  # Corrected path
     path('<str:room_name>/<str:username>/', views.MessageView, name='room'),
 ]
