125504b19fa78269d74ad5d07d151b02a2c40852
Piyush105454 <piyushmodi812@gmail.com>
2025-02-26 19:58:45 +0530

    Update 0001_initial.py

--- swapfood/migrations/0001_initial.py
+++ swapfood/migrations/0001_initial.py
@@ -1,12 +1,15 @@
 from django.db import migrations, models
 import django.db.models.deletion
+from django.conf import settings
 
 
 class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = []
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
 
     operations = [
         migrations.CreateModel(
@@ -22,22 +25,21 @@ class Migration(migrations.Migration):
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('sender', models.CharField(max_length=255)),
                 ('message', models.TextField()),
-                ('room', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='swapfood.room')),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('room', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='swapfood.room', related_name='messages')),
             ],
         ),
-       
         migrations.CreateModel(
             name='FoodPost',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('photo', models.ImageField(upload_to='food_photos/')),
-                ('latitude', models.FloatField()),
-                ('longitude', models.FloatField()),
+                ('latitude', models.FloatField(null=True, blank=True)),  # Fixed null=True
+                ('longitude', models.FloatField(null=True, blank=True)),  # Fixed null=True
                 ('name', models.CharField(max_length=100)),
-                ('description', models.CharField()),
-                ('location', models.CharField()),
+                ('description', models.CharField(max_length=255)),  # Fixed missing max_length
+                ('location', models.CharField(max_length=255)),  # Fixed missing max_length
                 ('radius', models.FloatField()),
-                
                 ('created_at', models.DateTimeField(auto_now_add=True)),
             ],
         ),
@@ -47,7 +49,7 @@ class Migration(migrations.Migration):
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('otp', models.CharField(max_length=6)),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to='auth.user')),
+                ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
         ),
     ]
