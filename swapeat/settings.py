4bfeeb26e74a0864c1ac59e50d34ba80e8265e67
Piyush105454 <piyushmodi812@gmail.com>
2025-02-27 09:58:13 +0530

    Update settings.py

--- swapeat/settings.py
+++ swapeat/settings.py
@@ -157,7 +157,7 @@ EMAIL_HOST = 'smtp.gmail.com'  # Use Gmail or any other email service provider
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'swapeatmail@gmail.com'  # Replace with your email
-EMAIL_HOST_PASSWORD = 'eylk dkix leuz zpyi'  # Replace with your app password
+EMAIL_HOST_PASSWORD = 'ptqs ondz fjfr wcuy'  # Replace with your app password
 DEFAULT_FROM_EMAIL = 'your_email@gmail.com'
 
 
