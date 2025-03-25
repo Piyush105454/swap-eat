b3b77a5999054b85c56b6010ce9c556b3ee7d464
Piyush Tamoli <piyushmodi812@gmail.com>
2025-03-25 06:08:13 +0530

    Update settings.py

--- swapeat/settings.py
+++ swapeat/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'swapfood',
     'channels',
+    'rest_framework',  # Django REST Framework for API
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
